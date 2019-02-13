573d9aa54948b1789af13491b2e802c002af7a1b
Stephen Ouyang <stephenouyang@Stephens-MacBook-Pro.local>
2019-02-13 13:46:33 -0800

    Users can sign up

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+require('dotenv').config();
+
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
+
+var cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 const express = require('express');
 const server = express();
 var exphbs = require('express-handlebars');
+
+server.use(cookieParser());
 
 server.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 server.set('view engine', 'handlebars')
@@ -18,6 +25,7 @@ server.use(expressValidator());
 require('./data/reddit-db');
 require('./controllers/posts.js')(server);
 require('./controllers/comments.js')(server);
+require('./controllers/auth.js')(server);
 
 server.listen(4000, () => {
   console.log("listening on port 4000")
